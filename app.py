1a7d297a6f400be8e0c7c535aea62f8063edb960
Jose Thomas Dominic <josethomasd@gmail.com>
2018-01-03 17:26:33 +0530

    Pickle add

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests
 from slackclient import SlackClient
 
 from flask import Flask, request, jsonify
+import pickle
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 sc = SlackClient(SLACK_BOT_TOKEN)
@@ -23,8 +24,20 @@ def verify():
     log(data)
     try:
     	if((data["event"]["type"]=='member_joined_channel') & (data["event"]["channel"]=="C69KJGBEJ")):
+    		try:
+    			pickle_off = open("Emp.pickle","rb")
+    		except Exception as f:
+    			emp = "xyz"
+    			pickling_on = open("Emp.pickle","wb")
+    			pickle.dump(emp, pickling_on)
+    			pickling_on.close()
+    			pickle_off = open("Emp.pickle","rb")
+    			log("Pickle fail")
+    		emp = pickle.load(pickle_off)
+    		event_id = data["event_id"]
     		team_id = data["event"]["channel"]
-    		return "ok", 200, send_greeting(team_id)
+    		if(event_id!=team_id):
+    			return "ok", 200, send_greeting(team_id)
     except Exception,e: 
         print str(e)
     return "ok", 200
@@ -43,8 +56,7 @@ def send_greeting(team_id):
 		text="Welcome! :tada:",
 		attachments=attachments
 		)
-
-    time.sleep(10)
+	# time.sleep(10)
 
 if __name__ == '__main__':
     app.run(debug=True)
